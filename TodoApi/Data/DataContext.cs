4cdf4e64bea22077d77c53505f1dbc5ee4434989
Lucas <lucas.levasseur18@gmail.com>
2024-11-06 14:52:09 -0300

    Feat DataContext

--- TodoApi/Data/DataContext.cs
+++ TodoApi/Data/DataContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Data.Common;
 using TodoApi.Models;
+using TodoApi.Utils;
 
 namespace TodoApi.Data
 {
@@ -13,5 +14,21 @@ namespace TodoApi.Data
 
         public DbSet<Usuario> usuarios { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            Usuario user = new Usuario();
+            Criptografia.CriarSenhaHash("123456789", out byte[] hash, out byte[] salt);
+            user.Id = 1;
+            user.Name = "Lucas";
+            user.Sobrenome = "Andres";
+            user.Email = "lucasapollar@gmail.com";
+            user.Senha_hash = hash;
+            user.Senha_salt = salt;
+            user.Senha = string.Empty;
+
+            modelBuilder.Entity<Usuario>().HasData(user);
+        }
     }
+
 }

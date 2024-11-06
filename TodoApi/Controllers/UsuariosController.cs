0b862e6f8e5ddc225acf21262271ed3cffd7c560
Lucas <lucas.levasseur18@gmail.com>
2024-11-06 14:52:26 -0300

    Feat UsuariosController

---
+++ TodoApi/Controllers/UsuariosController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Data;
+using TodoApi.Models;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Utils;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using System;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TodoApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private IConfiguration _configuration;
+
+        public UsuariosController(DataContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        private async Task<bool> UsuarioExistente(string email)
+        {
+            if (await _context.usuarios.AnyAsync(x => x.Email.ToLower() == email.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
